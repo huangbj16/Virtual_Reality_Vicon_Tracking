442cbaf11f6cbf06525aec23ff3b681e3ae3666c
Yeqi Sang <abelsang9683@gmail.com>
2022-12-28 23:02:06 -0500

    add init calibration data capture

--- Assets/Scripts/Calibration/InitialCalibration.cs
+++ Assets/Scripts/Calibration/InitialCalibration.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class InitialCalibration : MonoBehaviour
 {
   // Start is called before the first frame update
+
+  public class CalibrationData
+  {
+    public Vector3 viconDelta;
+    public Vector3 unityDelta;
+    public float difference;
+    public CalibrationData(Vector3 vicon, Vector3 unity, float diff)
+    {
+      this.viconDelta = vicon;
+      this.unityDelta = unity;
+      this.difference = diff;
+    }
+  }
   public Transform unityTransform;
 
   public Transform viconTransform;
 
-  private List<Vector3[]> dataCollection = new List<Vector3[]>();
+  private List<CalibrationData> dataCollection = new List<CalibrationData>();
+
+  public CalibrationData[] topThreeLeastDifference = new CalibrationData[3];
 
   private Vector3 previousViconPosition = Vector3.zero;
   private Vector3 previousUnityPosition = Vector3.zero;
 
-  private Matrix4x4 viconPointsMatrix = Matrix4x4.identity;
-  private Matrix4x4 unityPointsMatrix = Matrix4x4.identity;
 
-  private Matrix4x4 coordinatesRotationMatrix = Matrix4x4.identity;
+  public Matrix4x4 coordinatesRotationMatrix = Matrix4x4.identity;
 
 
   public void CaptureData()
@@ -27,27 +40,55 @@ public class InitialCalibration : MonoBehaviour
     if (unityPositionDelta.magnitude > 0.05 && viconPositionDelta.magnitude > 0.05)
     {
 
-      dataCollection.Add([unityPositionDelta.normalized, viconPositionDelta.normalized]);
-      Debug.Log(unityPositionDelta.magnitude + ", " + viconPositionDelta.magnitude + ", " + (unityPositionDelta.magnitude - viconPositionDelta.magnitude));
+      float difference = Mathf.Abs(unityPositionDelta.magnitude - viconPositionDelta.magnitude);
+      CalibrationData data = new CalibrationData(viconPositionDelta.normalized, unityPositionDelta.normalized, difference);
+      dataCollection.Add(data);
 
-      Debug.Log("unity pos change norm:" + Vector3.Normalize(unityPositionDelta).ToString("F4") + "vicon pos change norm: " + Vector3.Normalize(viconPositionDelta).ToString("F4"));
     }
     previousUnityPosition = unityTransform.position;
     previousViconPosition = viconTransform.position;
   }
 
-  public void JustDoIt()
+  private void UpdateInputMatrices(Matrix4x4 viconMatrix, Matrix4x4 unityMatrix, CalibrationData newData)
   {
-    Debug.Log("just do it ！");
+    if (newData.difference < topThreeLeastDifference[0].difference)
+    {
+      topThreeLeastDifference[2] = topThreeLeastDifference[1];
+      topThreeLeastDifference[1] = topThreeLeastDifference[0];
+      topThreeLeastDifference[0] = newData;
+    }
+    else if (newData.difference < topThreeLeastDifference[1].difference)
+    {
+      topThreeLeastDifference[2] = topThreeLeastDifference[1];
+      topThreeLeastDifference[1] = newData;
+    }
+    else if (newData.difference < topThreeLeastDifference[2].difference)
+    {
+      topThreeLeastDifference[2] = newData;
+    }
+  }
+
+  public void CalculateRotationMatrix()
+  {
+    Matrix4x4 viconMatrix = Matrix4x4.zero;
+    viconMatrix.SetRow(0, new Vector4(topThreeLeastDifference[0].viconDelta.x, topThreeLeastDifference[0].viconDelta.y, topThreeLeastDifference[0].viconDelta.z, 0));
+    viconMatrix.SetRow(1, new Vector4(topThreeLeastDifference[1].viconDelta.x, topThreeLeastDifference[1].viconDelta.y, topThreeLeastDifference[1].viconDelta.z, 0));
+    viconMatrix.SetRow(2, new Vector4(topThreeLeastDifference[2].viconDelta.x, topThreeLeastDifference[2].viconDelta.y, topThreeLeastDifference[2].viconDelta.z, 0));
+
+    Matrix4x4 unityMatrix = Matrix4x4.zero;
+    unityMatrix.SetRow(0, new Vector4(topThreeLeastDifference[0].unityDelta.x, topThreeLeastDifference[0].unityDelta.y, topThreeLeastDifference[0].unityDelta.z, 0));
+    unityMatrix.SetRow(1, new Vector4(topThreeLeastDifference[1].unityDelta.x, topThreeLeastDifference[1].unityDelta.y, topThreeLeastDifference[1].unityDelta.z, 0));
+    unityMatrix.SetRow(2, new Vector4(topThreeLeastDifference[2].unityDelta.x, topThreeLeastDifference[2].unityDelta.y, topThreeLeastDifference[2].unityDelta.z, 0));
+
+    Matrix4x4 inverseViconMatrix = viconMatrix.inverse;
+    coordinatesRotationMatrix = unityMatrix * inverseViconMatrix.transpose;
+
   }
   void Start()
   {
-
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
+    topThreeLeastDifference[0] = new CalibrationData(Vector3.zero, Vector3.zero, float.MaxValue - 2);
+    topThreeLeastDifference[1] = new CalibrationData(Vector3.zero, Vector3.zero, float.MaxValue - 1);
+    topThreeLeastDifference[2] = new CalibrationData(Vector3.zero, Vector3.zero, float.MaxValue);
 
   }
 
