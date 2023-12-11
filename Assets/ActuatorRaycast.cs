613ed85f8a3558db773c1957eb7ba576cbec328b
Bingjian Huang <huangbj16@gmail.com>
2023-12-11 17:23:07 -0500

    update Raycast script to include send command

--- Assets/ActuatorRaycast.cs
+++ Assets/ActuatorRaycast.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class ActuatorRaycast : MonoBehaviour
 {
+    public bool isTriggered = false;
     public Transform anchorPoint; // the anchor point which defines the direction
     public Transform point; // Your point with position and rotation
     private int motor_id = -1;
     private Material highlightMaterial;
     private Material defaultMaterial;
     private float maxDistance = 2.0f;
+
+    private GameObject senderObject;
+    private TcpSender sender;
+    public Dictionary<string, int> command;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +23,17 @@ public class ActuatorRaycast : MonoBehaviour
         motor_id = this.GetComponent<HapticMotor>().motor_id;
         highlightMaterial = Resources.Load<Material>("Materials/Material_Red");
         defaultMaterial = this.GetComponent<Renderer>().material;
+
+        senderObject = GameObject.Find("TCPSenderObject");
+        sender = senderObject.GetComponent<TcpSender>();
+        command = new Dictionary<string, int>()
+        {
+            { "addr", motor_id },
+            { "mode", 0 },
+            { "duty", 15 },
+            { "freq", 2 },
+            { "wave", 0 }
+        };
     }
 
     void Update()
@@ -35,17 +51,72 @@ public class ActuatorRaycast : MonoBehaviour
                 Debug.Log(this.name + motor_id + " Hit " + hit.collider.gameObject.name + ", Distance = " + hit.distance);
                 Debug.DrawRay(point.position, direction * hit.distance, Color.red);
                 this.GetComponent<Renderer>().material = highlightMaterial;
+                StartVibrate();
             }
             else
             {
                 Debug.DrawRay(point.position, direction * hit.distance, Color.white);
                 this.GetComponent<Renderer>().material = defaultMaterial;
+                StopVibrate();
             }
         }
         else
         {
             Debug.DrawRay(point.position, direction * maxDistance, Color.white);
             this.GetComponent<Renderer>().material = defaultMaterial;
+            StopVibrate();
+        }
+    }
+
+
+    public string DictionaryToString(Dictionary<string, int> dictionary)
+    {
+        string dictionaryString = "{";
+        foreach (KeyValuePair<string, int> keyValues in dictionary)
+        {
+            dictionaryString += "\"" + keyValues.Key + "\": " + keyValues.Value + ", ";
+        }
+        return dictionaryString.TrimEnd(',', ' ') + "}";
+    }
+
+    public void StartVibrate()
+    {
+        if (!isTriggered)
+        {
+            isTriggered = true;
+            // send TCP command
+            Debug.Log("Send start command to PORT:9051");
+            command["mode"] = 1;
+            string commandString = DictionaryToString(command) + "\n";
+            Debug.Log(commandString);
+            sender.SendData(commandString);
+            // collusionData = new CollusionData(motor_id, other);
+        }
+    }
+
+    public void Vibrate()
+    {
+        // trigger some animation.
+
+    }
+
+    public void StopVibrate()
+    {
+        if (isTriggered)
+        {
+            isTriggered = false;
+            //send TCP command
+            Debug.Log("Send stop command to PORT:9051");
+            command["mode"] = 0;
+            string commandString = DictionaryToString(command) + "\n";
+            Debug.Log(commandString);
+            sender.SendData(commandString);
+            sender.SendData(commandString);
+            // collusionData.detail = detail;
+            // collusionData.CalculateCollusionDuration();
+            // Debug.Log(collusionData.actutorId + ": " + other);
+            // shapeGenerator.currentUserData.TriggerAlert(collusionData);
+            // add event handler to trigger ohshape generator
         }
     }
 }
